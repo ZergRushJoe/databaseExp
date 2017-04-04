a5694610a4552a22efa787e853ecaf6107a667de
ppolicherla <pavan.policherla1991@gmail.com>
2017-04-04 10:48:36 -0400

    added the mongo router
    Pavan and Mikey added the mongo.js file that is serves as the router to
    the mongo database

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 
 const index = require('./routes/index');
+const mongo=require('./routes/mongo');/** calling the mongodb router*/
 
 let app = express();
 
@@ -21,7 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/mongo', mongo);
 app.use('/', index);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
