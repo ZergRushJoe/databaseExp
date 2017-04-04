a8756861d0a6b41bbd0fcfa165d6ce3b827f9b55
jstry1991 <jstry1991@gmail.com>
2017-04-04 11:54:25 -0400

    sqlite route
    -added new route folder for sqlite to display the items in the sqlite
    database.
    -added new link stylesheet to format the page containing the sqlite
    database
    -added app path for sqlite folder

--- app.js
+++ app.js
@@ -4,10 +4,11 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
-
+const sqlite = require('./routes/sqlite');
 const index = require('./routes/index');
 
 let app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +21,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/sqlite',sqlite);
 app.use('/', index);
 
 // catch 404 and forward to error handler
