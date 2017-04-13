7a6eea08a18ed3f7eea1061dab08ab2522035a22
mikeyho <ea7546@wayne.edu>
2017-04-13 15:15:33 -0400

    Created router to insert data
    Mikey created router to insert data.

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongo = require('mongodb');
-/** here we have the function that will pass the object to the database from an array that we pull*/
+
 let MongoClient = require('mongodb').MongoClient;
 let db;
 
@@ -12,26 +12,46 @@ MongoClient.connect('mongodb://127.0.0.1:27017/secProj', function(err, db_temp)
         console.log(err);
         return;
     }
-    db =db_temp;
+    db = db_temp;
 });
-/** Pavan and mikey have the data display on the webpage here. */
+
+/** Pavan and Mikey have the data display on the web page. */
+
 router.get("/items", function(req, res, next)
 {
     console.log(db.collection("items"));
-    let item_collection=db.collection("items");
+    let item_collection = db.collection("items");
 
     item_collection.find().toArray(function(err,rows)
     {
         console.log(rows);
         res.render('items', {items: rows})
     });
-
 });
+
 router.get("/insert",function(req,res,next)
 {
    res.render('forms');
 });
 
-module.exports = router;/**
- * Created by Pavan on 4/4/2017.
- */
+/** Created by Mikey on 4/13/17 */
+router.post("/insert", function(req, res, next)
+{
+    let item = {
+        name: req.body.name,
+        quantity: req.body.quantity
+    };
+    db.collection("items").insertOne(item, function(err, result)
+    {
+        if(err)
+        {
+            console.log(err);
+        }
+        else
+        {
+            console.log("Item inserted");
+        }
+    });
+});
+
+module.exports = router;
