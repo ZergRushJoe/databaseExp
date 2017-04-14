e0c388be91be9dc87c200d8a26eb0df8179cc82f
Joseph Karp <joe12345hd@yahoo.com>
2017-04-14 10:56:00 -0400

    scripte

--- public/javascripts/Submit.js
+++ public/javascripts/Submit.js
@@ -1,2 +1,43 @@
-alert("hello");
-console.log("hello");
+let button = document.getElementById('submitButton');
+let inputBox = document.getElementById('text');
+let body = document.getElementById('tbody');
+
+function search(e)
+{
+    let xhr = new XMLHttpRequest();
+    xhr.open('get','./search?name='+inputBox.value,true);
+    xhr.onreadystatechange = function()
+    {
+        if(xhr.status == 200 &&xhr.readyState === XMLHttpRequest.DONE )
+        {
+            let res = JSON.parse(xhr.response);
+            if(res.complete == true)
+            {
+                let items = res.items;
+                body.innerHTML = "";
+                if(items.length>0)
+                {
+
+                    for(let i = 0;i<items.length;i++)
+                    {
+                        body.innerHTML +=
+                            `
+                    <tr>
+                        <th class = "alignRight">${items[i].id || items[i].item_id}</th>
+                        <th class = "alignLeft">${items[i].name || items[i].item_name}</th>
+                        <th class = "alignLeft">${items[i].quantity}</th>
+                    </tr>
+                   `
+                    }
+                }
+            }
+            else
+            {
+                console.log(res.err);
+            }
+
+        }
+    };
+    xhr.send()
+}
+button.addEventListener("click",search,false);
