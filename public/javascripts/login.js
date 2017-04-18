413aeae965125ab6691c9dec57cc15ad7dd1ce29
knhumbarger <ev4295@wayne.edu>
2017-04-17 20:56:23 -0400

    Removed comments
    Sorry for talking to myself guys

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -13,10 +13,8 @@ function login(e)
     let xhr = new XMLHttpRequest();
     let encoded = encodeURIComponent('./login?username='+inputBoxUsername.value+'&password='+inputBoxPassword.value);
     xhr.open('get','./login?username='+inputBoxUsername.value+'&password='+inputBoxPassword.value,true);
-    console.log('just outside');
     xhr.onreadystatechange = function()
     {
-        console.log('but i made it here');
         if(xhr.status == 200 &&xhr.readyState === XMLHttpRequest.DONE )
         {
             let res = JSON.parse(xhr.response);
@@ -24,7 +22,6 @@ function login(e)
             if(res.complete == true)
             {
                 header.innerHTML = "<h3>"+res.items+"<\h3></\h3>";
-                console.log('yo Im here');
             }
             else
             {
